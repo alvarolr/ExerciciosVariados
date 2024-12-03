530e2b7d12b35ac14405f8492fa0242a83248050
Álvaro Lopes Rios <contatoalvarorios@gmail.com>
2024-12-03 09:53:45 -0500

    Create Exercicio11.py

---
+++ ExercicioListaPython/Exercicio11.py
@@ -0,0 +1,21 @@
+'''
+calculadora
+'''
+
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+def divisao(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erro: divisão por zero"
+
+print(soma(5, 3))
+print(divisao(10, 2))
