f50ee9dea1f8db330c84b9caf83eb7c19b92158a
Álvaro Lopes Rios <contatoalvarorios@gmail.com>
2025-01-14 13:20:10 -0500

    Update arquivo_Com_Pandas.py

--- arquivo_Com_Pandas.py
+++ arquivo_Com_Pandas.py
@@ -1,40 +1,23 @@
 import pandas as pd
 
-# Dados fictícios
-dados = {
-    "Produto": ["Arroz", "Feijão", "Macarrão", "Óleo", "Leite", 
-                "Refrigerante", "Cerveja", "Café", "Pão", "Bolo"],
-    "Categoria": ["Alimentos", "Alimentos", "Alimentos", "Alimentos", "Bebidas", 
-                  "Bebidas", "Bebidas", "Alimentos", "Padaria", "Padaria"],
-    "Quantidade Vendida": [150, 200, 120, 90, 300, 
-                           250, 180, 85, 400, 35],
-    "Preço Unitário": [5.50, 7.20, 4.30, 8.90, 3.40, 
-                       6.50, 4.20, 10.90, 1.20, 25.00]
-}
-
-# Criar DataFrame
-df = pd.DataFrame(dados)
-
-# Criar a coluna de receita
-df["Receita"] = df["Quantidade Vendida"] * df["Preço Unitário"]
-
-# Agrupamento por Categoria
-receita_por_categoria = df.groupby("Categoria")["Receita"].sum()
-
-# Salvar o resultado em um arquivo CSV
-receita_por_categoria.to_csv("receita_por_categoria.csv")
-
-# Salvar o resultado em um arquivo Excel
-receita_por_categoria.to_excel("receita_por_categoria.xlsx", sheet_name="Resumo")
-
-print("Arquivos salvos com sucesso!")
+#lendo o arquivo CSV
+df = pd.read_csv('dados_vendas.csv')
+print(df.head())
+#print(df.info())
+#print(df.describe())
+print(df['Produto'])
+maiores = df[df['Data de Venda'] > '2023-02-01']
+print(maiores)
 
 vendas_por_categorias = df.groupby('Categoria')['Quantidade Vendida'].sum()
 print(vendas_por_categorias)
 vendas_por_categorias = df.groupby(['Categoria', 'Produto'])['Quantidade Vendida'].sum()
 print(vendas_por_categorias)
 
+df['receita'] = df['Quantidade Vendida'] * df['Preço Unitário']
+
+receita_por_categoria = df.groupby('Categoria')['receita'].sum()
+print(receita_por_categoria)
+print(df.columns)
 vendas_por_categorias.to_csv('saida.csv', index=False)
                 
-
-
